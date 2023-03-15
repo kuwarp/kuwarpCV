a1d3e5d507e49625b1ec9e1408c20cb7e631c2aa
Kuwar Pratap Singh <guru75000@gmail.com>
2023-03-15 23:13:11 +0530

    Update index.js

--- src/UI/Projects/index.js
+++ src/UI/Projects/index.js
@@ -10,12 +10,7 @@ const Projects = ({ projectsList }) => {
           <Template title={title} desc={desc} link={link} liveLink={liveLink} />
         ))}
       </div>
-      <p className={styles.project_info_see_others}>
-        Check more at{" "}
-        <a href="https://tusharrajpoot.com/#/projects">
-          tusharrajpoot.com/projects
-        </a>
-      </p>
+    
     </div>
   );
 };
